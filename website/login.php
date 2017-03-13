b5b4e695d000b23a6a46b794b9e1b4eaa52f20a5
Philip <zeruxky.contact@yahoo.de>
2017-03-13 13:59:33 +0100

    Avoid mysql injections and support utf8-encoding

--- website/login.php
+++ website/login.php
@@ -1,6 +1,10 @@
 <?php
-$username = $_POST["username"];
-$password = $_POST["password"];
+$temp = trim($_POST['username']);
+$temp = mysql_real_escape_string($temp);
+$username = utf8_decode($temp);
+$temp = trim($_POST['password']);
+$temp = mysql_real_escape_string($temp);
+$password = utf8_decode($temp);
 $pass = hash ( "sha256" , $password , false );
 
 if($username == "Admin" AND $pass=="e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a")
